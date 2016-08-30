33abd88ec74df298dc6e3dc2218f52ad5287ec28
Charlie Chen <new791104@gmail.com>
2016-08-30 19:16:27 +0800

    update

---
+++ app/src/main/java/ccu/ant/countertable/Objects/Ring.java
@@ -0,0 +1,16 @@
+package ccu.ant.countertable.Objects;
+
+/**
+ * Created by Charlie on 2016/8/26.
+ */
+public class Ring {
+    int sid;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
